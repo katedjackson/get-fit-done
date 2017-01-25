1db5ce526631733768909ba188bfc673ea2017ee
Kathryn Middleton <kmiddleton14@gmail.com>
2017-01-25 17:31:23 -0500

    added a progress bar

--- popup/src/scripts/components/App.js
+++ popup/src/scripts/components/App.js
@@ -1,16 +1,50 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+require('rc-progress/assets/index.css');
+import { Line, Circle } from 'rc-progress';
+
+
 import Blocked from './Blocked';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state= {
+      percent: 0
+    };
   }
+
+
+  getInitialState() {
+     return {
+       percent: 0,
+     };
+   }
+
+   componentDidMount() {
+     this.increase();
+   } 
+
+   increase() {
+     let percent = this.state.percent + 1;
+     if (percent > 100) {
+       percent = 100;
+       clearTimeout(tm);
+       return;
+     }
+     this.setState({ percent });
+     var tm = setTimeout(this.increase, 10);
+   } 
+
 
   render() {
     return (
-      <Blocked />
+      <div style={{ margin: 10, width: 200 }}>
+        <Circle strokeWidth="6" percent={this.state.percent} />
+        
+        
+      </div>
     );
   }
 }
