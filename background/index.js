fa72ec2cf885eb72b5ff028f5bc077b47f3a49eb
Lisa Pan <l.pan4952@gmail.com>
2017-01-26 15:55:36 -0500

    all pages blocked

--- background/index.js
+++ background/index.js
@@ -1,23 +1,67 @@
-import React from 'react'
-import {render} from 'react-dom'
-import {browserHistory} from 'react-router'
-import App from 'components/app'
-import {trackView, trackTiming, trackException} from 'utils/analytics'
+'use strict';
 
-import _template from './index.html'
+//Retrieve user preferences, then execute a callback function that redirects to the "Stop wasting time page" if user navigates to a specified website during specified time.
+var startTime, endTime, startDay, endDay, websites, always;
+chrome.storage.sync.get({
+  // startTime: '',
+  // endTime: '',
+  // startDay: '',
+  // endDay: '',
+  // websites: '',
+  // always: ''
+}, function(items) {
+  // startTime = Number(items.startTime);
+  // endTime = Number(items.endTime);
+  // startDay = Number(items.startDay);
+  // endDay = Number(items.endDay);
+  // websites = items.websites;
+  // always = items.always;
 
-const init = () => {
-  render(<App history={browserHistory} />, document.querySelector('#container'))
+  //var urlsArray = websites.split(",").map(function(url){return "*://*." + url.trim() + "/*"});
+  //var urlsArray = ["*://*.facebook.com/*"]
 
-  trackView()
-  trackTiming('Blocked', 'Load', Date.now() - window.performance.timing.navigationStart)
-}
+  chrome.webRequest.onBeforeRequest.addListener(
+    function () {
+      // var currentTime = new Date();
+      // var hour = currentTime.getHours();
+      // var day = currentTime.getDay();
 
-const error = (e) => {
-  trackException(e.error)
-}
+      //if (always) {
+        return {redirectUrl: chrome.extension.getURL('background/index.html')};
+      // }
 
-/* ************************************************************************** */
+      // if (startTime < endTime && startDay <= endDay){
+      //   if ((hour >= startTime && hour < endTime) && (day >= startDay  && day <= endDay)) {
+      //     return {redirectUrl: chrome.extension.getURL('index.html')};
+      //   }
+      // }
 
-window.addEventListener('load', init, true)
-window.addEventListener('error', error, true)
+      // if (startTime > endTime && startDay <= endDay){
+      //   if ((hour >= startTime || hour < endTime) && (day >= startDay  && day <= endDay)) {
+      //     return {redirectUrl: chrome.extension.getURL('index.html')};
+      //   }
+      // }
+
+      // if (startTime < endTime && startDay > endDay){
+      //   if ((hour >= startTime && hour < endTime) && (day >= startDay || day <= endDay)) {
+      //     return {redirectUrl: chrome.extension.getURL('index.html')};
+      //   }
+      // }
+
+      // if (startTime > endTime && startDay > endDay){
+      //   if ((hour >= startTime || hour < endTime) && (day >= startDay || day <= endDay)) {
+      //     return {redirectUrl: chrome.extension.getURL('index.html')};
+      //   }
+      // }
+
+    },
+
+    {
+      urls: ["<all_urls>"],
+      types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+
+    ["blocking"]
+  );
+
+});
