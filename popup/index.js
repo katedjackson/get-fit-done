87b7753ab45fd53ed1cbbe81dcec87aa148aac4e
Kate <katelynj29@gmail.com>
2017-01-26 11:02:34 -0500

    oauth setheader added

--- popup/index.js
+++ popup/index.js
@@ -1,11 +1,20 @@
-const axios = require('axios');
-
 var fitbitAuth = new OAuth2('fitbit', {
   client_id: '2284DM',
   client_secret: '0f49bce2bd562b7f43657fc7431c8642',
   api_scope: 'activity, heartrate, location, nutrition, profile, settings, sleep, social, weight'
 });
 
+
 fitbitAuth.authorize(function() {
-  axios.head('https://api.fitbit.com/oauth2/token' + fitbitAuth.getAccessToken());
+  const signIn = document.getElementById('sign-in');
+
+  signIn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const xhr = new XMLHttpRequest();
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', 'OAuth ' + fitbitAuth.getAccessToken());
+  });
+
 });
+
+
