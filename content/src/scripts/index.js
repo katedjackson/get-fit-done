9a986b26b6d34ece1c6ebeb47b9ebefd47d966ec
emilymalecbrown <emilymalecbrown@gmail.com>
2017-01-30 15:42:39 -0500

    comments

--- content/src/scripts/index.js
+++ content/src/scripts/index.js
@@ -8,7 +8,7 @@ import App from './components/App';
 if (!document.getElementById('rcr-anchor')) {
   const anchor = document.createElement('div');
   anchor.id = 'rcr-anchor';
-
+  //what is react-chrome-redux doing for you in the long run? what is going to be on the store
   document.body.insertBefore(anchor, document.body.childNodes[0]);
   const proxyStore =  new Store({
     state: {},
--- popup/src/scripts/components/Unblocked.js
+++ popup/src/scripts/components/Unblocked.js
@@ -1,1 +1,2 @@
+//what is the plan here?
 Unblocked.js
