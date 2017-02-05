09805fd596ec8bd3c3692bc2dd00d10e962a6ef4
Lisa Pan <l.pan4952@gmail.com>
2017-02-05 14:35:44 -0500

    improving block logic

--- popup/components/Unblocked.js
+++ popup/components/Unblocked.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Icon } from 'semantic-ui-react';
+import { Circle } from 'react-progressbar.js'
 import Login from './Login';
 
 const Unblocked = (props) => {
+  const options = {
+      strokeWidth: 20,
+      trailWidth: 20,
+      easing: 'easeInOut',
+      duration: 1400,
+      text: {
+        autoStyleContainer: false
+      },
+      color: '#00B0B9'
+  };
+
+  const containerStyle = {
+      width: '200px',
+      height: '200px'
+  };
 
 
   return (
     <div>
-      Remaining time ....
+      <h3>{props.timeLeft} minutes to get your steps!</h3>
+      <Circle
+          progress = {(props.steps - props.lastSteps) / props.stepGoal}
+          text={`${(props.steps - props.lastSteps)}/${props.stepGoal}`}
+          options={options}
+          initialAnimate={true}
+          containerStyle={containerStyle}
+          containerClassName={'.progressbar'} />
       <Login />
     </div>
   );
 }
 
 const mapStateToProps = (state) => {
-  console.log('state: ', state);
+  console.log(state);
   return {
+    timeLeft: state.time && state.time.timeLeft,
+    accessToken: state.user && state.user.accessToken,
+    steps: state.user && state.user.steps,
+    lastSteps: state.user && state.user.lastSteps,
+    hourlySteps: state.user && state.user.hourlySteps,
+    stepGoal: state.settings && state.settings.stepGoal
   };
 };
 
