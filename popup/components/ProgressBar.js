aa119f10d0f781952b4ca339efd291a421b7afc1
Kathryn Middleton <kmiddleton14@gmail.com>
2017-02-07 17:06:42 -0500

    updated progress bar to stay completed once user completes their steps.  Also added a message to congratulate the user on reaching their goals

--- popup/components/ProgressBar.js
+++ popup/components/ProgressBar.js
@@ -21,23 +21,23 @@ const Blocked = (props) => {
       height: '200px'
   };
 
+
   return (
     <div>
       <Circle
-          progress = {(props.steps - props.lastSteps) / props.stepGoal}
+          progress = { ((props.steps - props.lastSteps)  >= props.stepGoal ) ? 1 : (props.steps - props.lastSteps) / props.stepGoal  }
           text={`${(props.steps - props.lastSteps)}/${props.stepGoal}`}
           options={options}
           initialAnimate={true}
           containerStyle={containerStyle}
           containerClassName={'.progressbar'} />
-
-
     </div>
   );
 }
 
 const mapStateToProps = (state) => {
   console.log('state: ', state);
+  console.log('TEST: ', (state.user.steps - state.user.lastSteps) / state.settings.stepGoal)
   return {
     accessToken: state.user && state.user.accessToken,
     steps: state.user && state.user.steps,
