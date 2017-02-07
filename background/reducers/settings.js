01e8caa491231a6d6604c56b22ebaaf5b878f3f6
Kate <katelynj29@gmail.com>
2017-02-07 18:33:54 -0500

    fixed  minor bugs

--- background/reducers/settings.js
+++ background/reducers/settings.js
@@ -19,7 +19,7 @@ const initialState = {
   // waterMode: false,
   // waterTime: [],
   sleepMode: true,
-  sleepTime: ['22:00', '6:00']
+  sleepTime: ['22:00', '06:00']
 }
 /*-------------------------ACTIONS----------------------------*/
 export const SET_WEBSITES = 'SET_WEBSITES';
