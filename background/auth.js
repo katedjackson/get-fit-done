309108224cf3954dec320230f2021fe11236c994
Sophia Bok <sophiatca@gmail.com>
2017-02-07 19:48:59 -0500

    consent

--- background/auth.js
+++ background/auth.js
@@ -7,13 +7,16 @@ const auth_url = 'https://www.fitbit.com/oauth2/authorize/?' +
     'redirect_uri=' + encodeURIComponent(redirect_uri) +
     '&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight' +
     '&expires_in=31536000' +
-    '&prompt=login';
-    //'&prompt=consent';
+    //'&prompt=login';
+    '&prompt=consent';
 
 export const fitbitAuth = () => {
+  console.log("auth,",auth_url)
   return new Promise(function (resolve, reject) {
     chrome.identity.launchWebAuthFlow({ 'url': auth_url, 'interactive': true},
       function(redirect_url) {
+
+        console.log("red",redirect_url)
          // extract the token from this url and use it for future requests
          let accessToken = redirect_url.substring(redirect_url.indexOf('=') + 1, redirect_url.indexOf('&user_id'));
          resolve(accessToken);
