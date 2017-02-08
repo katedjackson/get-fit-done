9851f4f938f18a1a0c45cff38b470026bd6d9594
Kate <katelynj29@gmail.com>
2017-02-08 11:47:53 -0500

    splash screen style

--- options/components/SplashScreen.js
+++ options/components/SplashScreen.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default function () {
   return (
     <div className="splash-screen">
-      <img src={"/logo.png"}/>
+      <img id="splash-logo" src={"/logo.png"}/>
       <h2>Stay fit and productive.</h2>
     </div>
   );
