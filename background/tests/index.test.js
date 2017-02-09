2e31acbc1332e23169e952a77a3ed5b8f94c62fd
Kathryn Middleton <kmiddleton14@gmail.com>
2017-02-09 18:19:08 -0500

    added a few more tests

--- background/tests/index.test.js
+++ background/tests/index.test.js
@@ -1,7 +1,7 @@
 
 import chai, {expect} from 'chai'                                                   
 
-import { setWebsites } from '../reducers/settings'
+import { setWebsites, setStepGoal, setTotalStepsTime } from '../reducers/settings'
 
 
 
@@ -19,6 +19,38 @@ describe('action creators', () => {
             expect(actionDescriptor).to.be.deep.equal({
                 type: 'SET_WEBSITES',
                 payload: websites
+            });
+
+        });
+    });
+
+    describe('setStepGoal', () => {
+
+        it('returns expected action description for setting step goal', () => {
+
+            const stepGoal = 200;
+
+            const actionDescriptor = setStepGoal(stepGoal);
+
+            expect(actionDescriptor).to.be.deep.equal({
+                type: 'SET_STEP_GOAL',
+                payload: stepGoal
+            });
+
+        });
+    });
+
+    describe('setTotalStepsTime', () => {
+
+        it('returns expected action description for setting total steps time', () => {
+
+            const totalStepTime = '12:00';
+
+            const actionDescriptor = setTotalStepsTime(totalStepTime);
+
+            expect(actionDescriptor).to.be.deep.equal({
+                type: 'SET_TOTAL_STEPS_TIME',
+                payload: totalStepTime
             });
 
         });
